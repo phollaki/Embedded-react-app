ec09158ad2c938b6e3f1f478ca8575660faea8e0
phollaki <phollakos15@gmail.com>
2023-07-10 13:29:50 +0200

    create an toggleable overlay that fills out the whole page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,26 @@
+import React, {useState} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [showOverlay, setShowOverlay] = useState(false)
+  console.log(showOverlay)
   return (
     <div className="App">
+      {showOverlay && <div className='App-overlay'>overlay content</div>}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={()=>setShowOverlay(true)}
         >
-          Learn React
-        </a>
+          Open overlay
+        </button>
       </header>
     </div>
   );
